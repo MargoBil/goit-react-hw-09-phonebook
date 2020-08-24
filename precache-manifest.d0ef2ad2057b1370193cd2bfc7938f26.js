a26deaae4db311fc33f90ff4d37ef48316a78589
margo10011990 <margo10011990@gmail.com>
2020-08-24 14:22:50 +0300

    Updates

---
+++ precache-manifest.d0ef2ad2057b1370193cd2bfc7938f26.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b8080f3ecb4766cf0f08ffd71058c5",
+    "url": "/goit-react-hw-09-phonebook/index.html"
+  },
+  {
+    "revision": "e5b49111bb66cf67fd3f",
+    "url": "/goit-react-hw-09-phonebook/static/css/ContactsPage.0caf1e1a.chunk.css"
+  },
+  {
+    "revision": "ab467b5859152eb65e81",
+    "url": "/goit-react-hw-09-phonebook/static/css/SignInPage.db086036.chunk.css"
+  },
+  {
+    "revision": "9e1ae190c9e4db572061",
+    "url": "/goit-react-hw-09-phonebook/static/css/SignUpPage.db086036.chunk.css"
+  },
+  {
+    "revision": "0afab171bcb4bdc45635",
+    "url": "/goit-react-hw-09-phonebook/static/css/main.2ecbbf12.chunk.css"
+  },
+  {
+    "revision": "c107a847f44d93430e98",
+    "url": "/goit-react-hw-09-phonebook/static/js/5.25c30d2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-09-phonebook/static/js/5.25c30d2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b49111bb66cf67fd3f",
+    "url": "/goit-react-hw-09-phonebook/static/js/ContactsPage.020e3e78.chunk.js"
+  },
+  {
+    "revision": "ab467b5859152eb65e81",
+    "url": "/goit-react-hw-09-phonebook/static/js/SignInPage.29d6e9c8.chunk.js"
+  },
+  {
+    "revision": "9e1ae190c9e4db572061",
+    "url": "/goit-react-hw-09-phonebook/static/js/SignUpPage.fc281889.chunk.js"
+  },
+  {
+    "revision": "0afab171bcb4bdc45635",
+    "url": "/goit-react-hw-09-phonebook/static/js/main.f6e7213b.chunk.js"
+  },
+  {
+    "revision": "9253fa3a26220bdeda16",
+    "url": "/goit-react-hw-09-phonebook/static/js/runtime-main.7aed719f.js"
+  }
+]);
